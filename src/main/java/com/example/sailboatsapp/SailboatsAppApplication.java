a07f51e36d93dd217c1b59667dac7b51cd07cce2
BorkowskiMaciej <maciej.borkowski@op.pl>
2023-12-15 15:45:35 +0100

    initial commit

---
+++ src/main/java/com/example/sailboatsapp/SailboatsAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.sailboatsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SailboatsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SailboatsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sailboatsapp/SailboatsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sailboatsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SailboatsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
